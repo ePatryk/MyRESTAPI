95346b30b12edc95069adbc83be25feaeff43967
ePatryk <pat.kal1989@gmail.com>
2023-02-13 15:44:02 +0100

    POST file

---
+++ POST.py
@@ -0,0 +1,20 @@
+import requests
+
+url = "http://localhost:5000/products"
+
+headers = {
+    "Content-Type": "application/json"
+}
+
+data = {
+    "name": "Product 4",
+    "price": 49.99
+}
+
+response = requests.post(url, headers=headers, json=data)
+
+if response.status_code == 201:
+    print("Product created successfully")
+else:
+    print("Failed to create product")
+# print(response.status_code)
